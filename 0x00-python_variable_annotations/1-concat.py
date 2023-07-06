73af89f9ead7c115ed9f360a14d1321d2157942e
Felixdiamond <diamondfelix006@gmail.com>
2023-07-06 09:12:53 -0700

    python variable annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This code takes two strings as input and returns a concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns the concatenated string"""
+    return str1 + str2
