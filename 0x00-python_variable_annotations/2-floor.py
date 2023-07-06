82a76b623ee84f726f9948149fa06ecd3994b92b
Felixdiamond <diamondfelix006@gmail.com>
2023-07-06 09:26:49 -0700

    python variable annotations

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """This code takes a float as input and returns the floor of the float"""
+import math
 
 
 def floor(n: float) -> int:
     """Returns the floor of the float"""
-    return int(n // 1)
+    return math.floor(n)
