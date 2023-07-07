75eff95f1bef78f82054150561fb1a21e36e841b
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2023-07-07 03:38:49 +0100

    Update 9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
-"""Define a type-annotated function element_length"""
+"""This module defines a type-annotated function element_length that takes an iterable of sequences as argument and returns a list of tuples."""
+
 from typing import Iterable, List, Sequence, Tuple
 
-
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """Takes an iterable of sequences as argument and returns a list of tuples."""
+    """Takes an iterable of sequences as argument and returns a list of tuples.
+
+    Args:
+        lst: An iterable of sequences.
+
+    Returns:
+        A list of tuples containing each element of lst as the first element and the length of that element as the second element.
+    """
     return [(i, len(i)) for i in lst]
