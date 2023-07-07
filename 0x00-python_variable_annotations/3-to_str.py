2fc2ba5f651bf7c888cf43eb30bff26dde9b74f4
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2023-07-07 03:22:30 +0100

    Update 3-to_str.py

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """This code takes a float as input and return the string representation of this float"""
-
 def to_str(n: float) -> str:
     """
     Takes a float n as argument and returns the string representation of the float.
