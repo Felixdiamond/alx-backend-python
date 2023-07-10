8915f76b186590f436d61efec44b9ec387d107aa
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2023-07-10 23:19:07 +0100

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,9 @@
-#!/usr/bin/env python3
-"""Asynchronous coroutine that waits for multiple random delays and returns
-them."""
-
 import asyncio
 from typing import List
-from 0 - basic_async_syntax import wait_random
-
-wait_random = __import__('0-basic_async_syntax').wait_random
-
+from 0-basic_async_syntax import wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Asynchronous coroutine that waits for n random delays between 0 and
-    max_delay seconds and returns them."""
+    """Asynchronous coroutine that waits for n random delays between 0 and max_delay seconds and returns them."""
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = [await task for task in asyncio.as_completed(tasks)]
     return delays
