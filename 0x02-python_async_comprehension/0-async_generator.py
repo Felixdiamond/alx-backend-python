0b0ac13081bb3f61c4c34ab3eb271b95d6d17984
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2023-07-11 21:34:34 +0100

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,16 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """Coroutine that yields random numbers between 0 and 10"""
+        """Asynchronously generate random numbers between 0 and 10.
+
+    This coroutine generates a sequence of 10 random numbers between 0 and 10.
+    On each iteration of the loop, the coroutine asynchronously waits for 1 second
+    before yielding the next random number. The random numbers are generated using
+    the uniform function from the random module.
+
+    Yields:
+        float: A random number between 0 and 10.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
