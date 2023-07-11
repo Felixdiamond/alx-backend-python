11ba17198323c345851aeafb4fe670dfedab7c91
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2023-07-11 21:48:49 +0100

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-"""Example script that uses the async_generator coroutine"""
+"""A script that uses the async_generator coroutine"""
 
-import asyncio
-import random
+from asyncio import sleep
+from random import uniform
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
+
     """Asynchronously generate random numbers between 0 and 10.
 
     This coroutine generates a sequence of 10 random numbers between 0 and 10.
@@ -18,12 +19,5 @@ async def async_generator() -> Generator[float, None, None]:
         float: A random number between 0 and 10.
     """
     for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
-
-
-async def print_yielded_values():
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    print(result)
+        await sleep(1)
+        yield uniform(0, 10)
