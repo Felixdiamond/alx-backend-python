00f9b29939363ed2975551f3e2dcebd73793b07b
Felixdiamond <diamondfelix006@gmail.com>
2023-07-29 13:00:19 -0700

    unittests and integrated tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""This module contains the TestAccessNestedMap class for testing the utils.access_nested_map function."""
+"""This module contains the TestAccessNestedMap class for testing the
+utils.access_nested_map function.
+"""
 
 import unittest
 from parameterized import parameterized
@@ -14,7 +16,11 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map: dict, path: tuple, expected_output: any) -> None:
-        """Tests that the utils.access_nested_map function returns the expected output for given inputs."""
-        self.assertEqual(utils.access_nested_map(nested_map, path), expected_output)
+    def test_access_nested_map(self, nested_map: dict, path: tuple,
+                               expected_output: any) -> None:
+        """Tests that the utils.access_nested_map function returns the expected
+        output for given inputs.
+        """
+        self.assertEqual(utils.access_nested_map(nested_map, path),
+                         expected_output)
 
