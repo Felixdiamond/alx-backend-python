4e25a7d20f3f75c74d99139c68207df9095c92b8
Felixdiamond <diamondfelix006@gmail.com>
2023-07-31 00:51:40 -0700

    unittests and integrated tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,25 +3,27 @@
 utils.access_nested_map function.
 """
 
-from typing import Any, Dict, Tuple
 import unittest
 from parameterized import parameterized
 import utils
+from typing import Any, Dict, Tuple
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """This class contains test cases for the utils.access_nested_map
-    function."""
+    function.
+    """
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map: dict, path: tuple,
-                               expected_output: any) -> None:
-        """Tests that the utils.access_nested_map function returns the expected
-        output for given inputs.
+    def test_access_nested_map(self, nested_map: Dict[str, Any], 
+                               path: Tuple[str, ...],
+                               expected_output: Any) -> None:
+        """Tests that the utils.access_nested_map function returns the
+        expected output for given inputs.
         """
         self.assertEqual(
             utils.access_nested_map(nested_map, path),
@@ -41,3 +43,18 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(KeyError):
             utils.access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Tests the get_json function from the utils module"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @unittest.mock.patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Tests get_json with different inputs"""
+        mock_get.return_value.json.return_value = test_payload
+        self.assertEqual(utils.get_json(test_url), test_payload)
+        mock_get.assert_called_once_with(test_url)
