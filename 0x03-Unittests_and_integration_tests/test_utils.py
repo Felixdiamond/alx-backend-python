fccaf6ab98807ab05ab59a884a887531a183bc56
Felixdiamond <diamondfelix006@gmail.com>
2023-07-31 00:55:50 -0700

    unittests and integrated tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,25 +10,23 @@ from typing import Any, Dict, Tuple
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """This class contains test cases for the utils.access_nested_map
-    function.
-    """
+    """This class contains test cases for the utils.access_nested_map function."""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map: Dict[str, Any],
-                               path: Tuple[str, ...],
-                               expected_output: Any) -> None:
-        """Tests that the utils.access_nested_map function returns the
-        expected output for given inputs.
+    def test_access_nested_map(self, nested_map: dict, path: tuple,
+                               expected_output: any) -> None:
+        """Tests that the utils.access_nested_map function returns the expected
+        output for given inputs.
         """
         self.assertEqual(
             utils.access_nested_map(nested_map, path),
             expected_output
         )
+    
 
     @parameterized.expand([
         ({}, ("a",)),
@@ -52,6 +50,7 @@ class TestGetJson(unittest.TestCase):
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
+
     @unittest.mock.patch('requests.get')
     def test_get_json(self, test_url, test_payload, mock_get):
         """Tests get_json with different inputs"""
