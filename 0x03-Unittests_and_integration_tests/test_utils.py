ff1a5e2ac31c05e5e0880535068f3b6d798c378a
Felixdiamond <diamondfelix006@gmail.com>
2023-07-31 03:49:22 -0700

    unittests and integrated tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -68,7 +68,8 @@ class TestMemoize(unittest.TestCase):
     """Tests the memoize decorator from the utils module"""
 
     def test_memoize(self):
-        """Tests that the memoize decorator correctly caches the result of a method"""
+        """Tests that the memoize decorator correctly caches the result of
+        a method"""
         class TestClass:
             def a_method(self):
                 return 42
@@ -77,7 +78,8 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+        with patch.object(TestClass, 'a_method',
+                          return_value=42) as mock_method:
             test_class = TestClass()
             self.assertEqual(test_class.a_property, 42)
             self.assertEqual(test_class.a_property, 42)
