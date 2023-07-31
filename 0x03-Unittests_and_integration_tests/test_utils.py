034ad228ce455bc5aa82b325d74388d82ba6e502
Felixdiamond <diamondfelix006@gmail.com>
2023-07-31 03:43:48 -0700

    unittests and integrated tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,6 +8,7 @@ from unittest.mock import patch
 from parameterized import parameterized
 import utils
 from utils import get_json
+from utils import memoize
 from typing import Any, Dict, Tuple
 
 
@@ -61,3 +62,23 @@ class TestGetJson(unittest.TestCase):
         self.assertEqual(get_json(test_url), test_payload)
         mock.assert_called_once_with(test_url)
         patcher.stop()
+
+
+class TestMemoize(unittest.TestCase):
+    """Tests the memoize decorator from the utils module"""
+
+    def test_memoize(self):
+        """Tests that the memoize decorator correctly caches the result of a method"""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property, 42)
+            self.assertEqual(test_class.a_property, 42)
+            mock_method.assert_called_once()
